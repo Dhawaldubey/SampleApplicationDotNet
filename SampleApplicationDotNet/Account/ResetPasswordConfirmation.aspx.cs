61b69305bda3ce6b925d6ececf41fa4cad2f393c
dhawaldubey <dhawaldubey@gmail.com>
2018-08-31 12:18:50 +0530

    first commit

---
+++ SampleApplicationDotNet/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SampleApplicationDotNet.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SampleApplicationDotNet/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleApplicationDotNet.Startup))]
+namespace SampleApplicationDotNet
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
